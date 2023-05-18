71bca32b8218cf780a5a10c4849cefca606660f1
Kundan Kumar <59140242+kundan-6646@users.noreply.github.com>
2023-05-18 16:32:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 function palindrome(str){
 	let i = 0, j = str.length-1;
 	while(i < j) {
-		if(str.charAt(i) != str.charAt(j)) return false;
+		if(str.charAt(i).toLowerCase() != str.charAt(j).toLowerCase()) return false;
 		i++;
 		j--;
 	}
 	return true;
 }
+
+alert(palindrome('abbababababababa'));
 
 module.exports = palindrome
